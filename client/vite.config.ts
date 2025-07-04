7f7536e27b2644df9f1461bf968985241bccadb9
ethanmchan <ethanmchan@gmail.com>
2025-07-03 23:34:52 -0400

    added music icon and changed tab image

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
 plugins: [
-react(), tailwindcss()
+react(), tailwindcss(), svgr()
 ],
 })
+
